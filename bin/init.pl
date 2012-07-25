9368fd704755a39ecdf200f816d17418502e9cd8
Ryan Whitehurst <ryan@opensourcery.com>
2012-07-25 16:18:30 -0700

    Add schema path to bin/init.pl

--- bin/init.pl
+++ bin/init.pl
@@ -1,10 +1,18 @@
 #!/usr/bin/env perl
 
+use strict;
+use warnings;
+
 use YAML::Tiny;
 use Data::Dumper;
 
-# Open $files_path/conf/schema.yml
-$conf = YAML::Tiny->new;
-$conf = YAML::Tiny->read( $files_path . '/conf/schema.yml' );
+# Fix this to guess, prompt, or get from parameter.
+my $schema_path = 'conf/schema.yml';
 
-$files_path = $conf->[0]->{ 'info' }->{ 'base_dir' };
+# Open $schema_path
+my $conf = YAML::Tiny->new;
+$conf = YAML::Tiny->read( $schema_path )->[0];
+
+my $files_path = $conf->{ info }->{ base_dir };
+
+print Dumper $conf;
