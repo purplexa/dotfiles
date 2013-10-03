ffafd6df022c48a9dd9cf6a6674bb10d67ed163b
Ryan Whitehurst <ryan@opensourcery.com>
2013-10-03 08:15:02 -0700

    Add script to build a drupal site (vhosts, etc)

---
+++ conf/default.settings.local.php
@@ -0,0 +1,16 @@
+<?php
+$databases = array(
+  'default' => array(
+    'default' => array(
+      'database' => 'PROJECTNAME',
+      'username' => 'PROJECTNAME',
+      'password' => 'PROJECTNAME',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
+#$conf['mail_system'] = array( 'default-system' => 'DevelMailLog', );
