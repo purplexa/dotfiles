9f62c2bc2378c0a18915007bcb8fc261838ef6ac
Ryan Whitehurst <ryan@opensourcery.com>
2014-09-10 11:25:45 -0700

    Update default settings.local.php

--- conf/default.settings.local.php
+++ conf/default.settings.local.php
@@ -13,4 +13,10 @@ $databases = array(
   ),
 );
 
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+$conf['error_level'] = 2;
+
 #$conf['mail_system'] = array( 'default-system' => 'DevelMailLog', );
