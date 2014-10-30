96db7e20ffd6359586d896bdacae747c4be83840
Ryan Whitehurst <rw@puppetlabs.com>
2014-10-30 10:15:55 -0700

    Add some more packages

--- lib/emacs/custompackages.el
+++ lib/emacs/custompackages.el
@@ -1,21 +1,22 @@
 ;; Packages
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+             '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
 (defvar user-custom-packages
-  '(apache-mode auto-complete browse-kill-ring
-                color-theme-sanityinc-tomorrow drupal-mode evil
-                expand-region feature-mode flycheck flymake-css
-                flymake-jslint flymake-php flymake-sass flx-ido
-                git-commit-mode gitconfig-mode geben god-mode
-                haskell-mode highlight-symbol js3-mode leuven-theme
-                markdown-mode multiple-cursors php-auto-yasnippets
-                php-mode powerline projectile puppet-mode
-                rainbow-delimiters scss-mode smex smooth-scrolling
-                solarized-theme ssh-config-mode switch-window
-                web-mode wrap-region yaml-mode yasnippet))
+  '(ack-and-a-half aggressive-indent-mode apache-mode auto-complete
+                   base16-theme browse-kill-ring buffer-move
+                   color-theme-sanityinc-tomorrow drupal-mode evil
+                   expand-region feature-mode flycheck flymake-css
+                   flymake-jslint flymake-php flymake-sass flx-ido
+                   git-commit-mode gitconfig-mode geben god-mode
+                   haskell-mode highlight-symbol js3-mode leuven-theme
+                   markdown-mode multiple-cursors php-auto-yasnippets
+                   php-mode powerline projectile puppet-mode
+                   rainbow-delimiters scss-mode smex smooth-scrolling
+                   solarized-theme ssh-config-mode switch-window
+                   web-mode wrap-region yaml-mode yasnippet))
 
 (defun custom-packages-installed ()
   (loop for p in user-custom-packages
