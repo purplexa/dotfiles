24c509fae35eda6df9669921ee9e125ff25b6489
Ryan Whitehurst <rw@puppetlabs.com>
2014-11-06 16:05:02 -0800

    Update package list

--- lib/emacs/custompackages.el
+++ lib/emacs/custompackages.el
@@ -1,22 +1,23 @@
 ;; Packages
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/"))
+             '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
 (defvar user-custom-packages
-  '(ack-and-a-half aggressive-indent-mode apache-mode auto-complete
+  '(ack-and-a-half aggressive-indent apache-mode auto-complete
                    base16-theme browse-kill-ring buffer-move
                    color-theme-sanityinc-tomorrow drupal-mode evil
                    expand-region feature-mode flycheck flymake-css
                    flymake-jslint flymake-php flymake-sass flx-ido
                    git-commit-mode gitconfig-mode geben god-mode
                    haskell-mode highlight-symbol js3-mode leuven-theme
-                   markdown-mode multiple-cursors php-auto-yasnippets
-                   php-mode powerline projectile puppet-mode
-                   rainbow-delimiters scss-mode smex smooth-scrolling
-                   solarized-theme ssh-config-mode switch-window
-                   web-mode wrap-region yaml-mode yasnippet))
+                   markdown-mode moe-theme multiple-cursors
+                   php-auto-yasnippets php-mode powerline projectile
+                   puppet-mode rainbow-delimiters scss-mode smex
+                   smooth-scrolling solarized-theme ssh-config-mode
+                   switch-window web-mode wrap-region yaml-mode
+                   yasnippet))
 
 (defun custom-packages-installed ()
   (loop for p in user-custom-packages
