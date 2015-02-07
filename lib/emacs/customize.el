a051843c0cb6e5e73461572d8654db272d61963a
Ryan Whitehurst <rw@puppetlabs.com>
2015-02-07 14:10:19 -0800

    Use light grey for highlighting indentation levels

--- lib/emacs/customize.el
+++ lib/emacs/customize.el
@@ -69,6 +69,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(highlight-indentation-face ((t (:background "gray21"))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "wheat"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "cornflower blue"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "light green"))))
